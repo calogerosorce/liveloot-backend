6f62e7c543d89ff8f9401daa798638b8fbc4869a
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2026-01-10 16:40:44 +0100

    rimosso file .env.example, aggiornato user in connection.js e aggiunti file .DS_Store a .gitignore

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
