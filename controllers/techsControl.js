160686d63bb70b9d1fbe81e67926a5135825c1c3
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-01-15 10:01:25 +0100

    Ambrogio aggiunge stripe e la sua rotta

--- controllers/techsControl.js
+++ controllers/techsControl.js
@@ -1,5 +1,7 @@
 const { text } = require('express')
 const connection = require('../database/connection')
+const Stripe = require('stripe')
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const index = (req, res) => {
 
@@ -134,6 +136,23 @@ const searchProducts = async (req, res) => {
     }
 };
 
+const makePayment = async (req, res) => {
+    const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        //DA INSERIRE QUI amount: ,
+        currency: "eur",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+};
 
 
 
@@ -144,5 +163,6 @@ module.exports = {
     show,
     indexAll,
     showSingle,
-    searchProducts
+    searchProducts,
+    makePayment
 }
